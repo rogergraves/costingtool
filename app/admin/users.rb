6e086660b0ce12dd7ee03a6b48bb505bb0f3c7b4
Roger Graves <rogergraves@gmail.com>
2013-04-17 16:08:08 -0700

    Fixed adding new users via activeadmin gem [#48210457]

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,22 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+
+  filter :email
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      if f.object.new_record?
+        f.input :password
+        f.input :password_confirmation
+      end
+    end
+    f.actions
+  end
 end
