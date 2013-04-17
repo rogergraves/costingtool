d1ea7371de17503a7804b8909faac07b9a7b9c67
Roger Graves <rogergraves@gmail.com>
2013-04-17 16:39:52 -0700

    Made the show page look better with only relevant fields [finishes #48210457]

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,6 +7,15 @@ ActiveAdmin.register User do
     default_actions
   end
 
+  show do |ad|
+    attributes_table do
+      row :email
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :sign_in_count
+    end
+  end
+
   filter :email
 
   form do |f|
