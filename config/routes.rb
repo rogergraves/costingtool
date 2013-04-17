ab1f448cddbb5c59250570f216b5a8c0878c3b6f
Ben Johnston <benjohnstonsf@gmail.com>
2013-04-17 11:35:11 -0700

    Added activeadmin admin framework, login at '/admin'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Costingtool::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
+  ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
