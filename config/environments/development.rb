fa3cb8aab10b876d61e459b138b1d857be0eace4
Roger Graves <rogergraves@gmail.com>
2013-04-16 18:41:21 -0700

    Added devise under Rails 3.2 [#48209985 #48138135]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Costingtool::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Devise told me ot do this...
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,8 @@ Costingtool::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  # Devise insisted I do this
+  root :to => "home#index"
+
 end
