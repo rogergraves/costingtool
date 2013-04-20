6335c3f4f1a0de1813f3729b2bbe839b9c2e093e
Roger Graves <rogergraves@gmail.com>
2013-04-19 17:36:56 -0700

    Another fix attempt for S3, this time the region end point [#48369295]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Costingtool::Application.configure do
           :bucket => ENV['S3_BUCKET_NAME'],
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
+      },
+      s3_host_name: 's3-ap-southeast-1.amazonaws.com'
   }
 end
