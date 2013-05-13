1398d44ea66b630eeb28f97d6f30ab4a11649238
ea0723 <elenanewton@icloud.com>
2013-05-13 10:50:58 -0600

    wip clicks_lookup_table in admin page

--- app/admin/clicks_lookup_tables.rb
+++ app/admin/clicks_lookup_tables.rb
@@ -1,31 +1,51 @@
 ActiveAdmin.register ClicksLookupTable do
   sidebar "Clicks Lookup Entries" do
     ul do
-      li link_to("Create entries", new_admin_clicks_lookup_table_clicks_lookup_path(clicks_lookup_table))
-
-      if clicks_lookup_table.clicks_lookups.present?
-        li link_to("Edit entries", admin_clicks_lookup_table_clicks_lookup_path(clicks_lookup_table))
-      end
+      #li link_to("Create entries", new_admin_clicks_lookup_table_clicks_lookup_path(clicks_lookup_table))
+      #
+      #if clicks_lookup_table.clicks_lookups.present?
+      #  li link_to("Edit entries", admin_clicks_lookup_table_clicks_lookup_path(clicks_lookup_table))
+      #end
     end
   end
 
   index do
-    column :description
-
+    column :click_description
+    column :tier_label
+    column :color_range_start
+    column :color_range_end
+    column :black
+    column :volume_range_start
+    column :volume_range_end
+    column :price
     default_actions
   end
 
   show do |press|
     attributes_table do
-      row :description
+      row :click_description
+      row :tier_label
+      row :color_range_start
+      row :color_range_end
+      row :black
+      row :volume_range_start
+      row :volume_range_end
+      row :price
     end
   end
 
-  filter :description
+  filter :click_description
 
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "Details" do
-      f.input :description
+      f.input :click_description
+      f.input :tier_label
+      f.input :color_range_start, :as => :select, :collection => ["0", "1", "2", "3", "4", "5", "6"]
+      f.input :color_range_end, :as => :select, :collection => ["0", "1", "2", "3", "4", "5", "6"]
+      f.input :black, :as => :radio, :collection => {"Yes" => 1, "No" => 0}
+      f.input :volume_range_start, :as => :number
+      f.input :volume_range_end, :as => :number
+      f.input :price, :as => :number, :label => "Price (USD)"
     end
 
     f.buttons
