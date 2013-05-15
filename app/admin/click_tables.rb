8752bd6ca978c99b7b334e9f96710414fb3a8a17
ea0723 <elenanewton@icloud.com>
2013-05-15 01:51:58 -0700

    removed create_tiers rake file and created click_tables resource using ActiveAdmin [WIP #48791543]

--- app/admin/click_tables.rb
+++ app/admin/click_tables.rb
@@ -3,4 +3,12 @@ ActiveAdmin.register ClickTable do
     column :description
     default_actions
   end
+
+  show do
+    attributes_table do
+      row :ink_array
+      row :description
+    end
+  end
+
 end
