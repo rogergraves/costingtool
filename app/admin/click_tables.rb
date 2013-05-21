5f1ae0fc5fafc8b8ae3bc7bdb4991eebb100beff
ea0723 <elenanewton@icloud.com>
2013-05-21 14:23:38 -0700

    view ink arrays and tiers simultaneously. Need to finish up breadcrumbs for tiers (see app/admin/ink_arrays.rb [WIP #50301491]

--- app/admin/click_tables.rb
+++ app/admin/click_tables.rb
@@ -1,23 +1,8 @@
 ActiveAdmin.register ClickTable do
 
-  sidebar "Links", :only => :show do
-    ul do
-      li link_to "See All Click Tables", admin_click_tables_path()
-      li link_to "Create a New Click Table", new_admin_click_table_path()
-      li link_to "Add an Ink Array", new_admin_click_table_ink_array_path(click_table.id)
-      #li link_to "Add a New Tier", admin_click_table_ink_arrays_path(click_table.id)
-    end
-  end
-
-  filter :click_table
-  filter :ink_array
-  filter :tier
-
   index do
-    selectable_column
-    column :click_table_name, :label => "Name"
+    column(:click_table_name, :sortable => :click_table_name) { |click_table| link_to click_table.click_table_name, admin_click_table_path(click_table.id) }
     column :description
-    default_actions
   end
 
   form do |f|
@@ -36,26 +21,20 @@ ActiveAdmin.register ClickTable do
 
     panel "Ink Arrays" do
       table_for(InkArray.find_all_by_click_table_id(click_table.id)) do
-        column(:description) {|ink_array| link_to ink_array.description, edit_admin_click_table_ink_array_path(ink_array.click_table_id, ink_array.id) }
-        #column :description, :label => 'Ink Levels'
+        column(:description) {|ink_array| link_to ink_array.description, admin_click_table_ink_array_path(ink_array.click_table_id, ink_array.id) }
         column :color_range_start
         column :color_range_end
         column :black
         end
-        #button link_to "New", new_admin_click_table_ink_array
+        button_to("Add New Ink Array"){|ink_array| link_to ink_array.description, new_admin_click_table_ink_array_path(ink_array.click_table_id) }
     end
 
   end
 
-  #def inkarrays_by_ctid
-  #  InkArray.find_all_by_click_table_id(click_table.id)
-  #end
-  #
-  #def tiers_by_iaid
-  #  Tier.find_all_by_ink_array_id(InkArray.find_all_by_click_table_id(click_table.id))
-  #  #trying to find all tiers for all ink arrays for this click_table id ()
-  #end
-
+  sidebar "Links", :only => :show do
+      #button_to "Add an Ink Array", edit_admin_click_table_ink_array_path(click_table.id, ink_array.id )
+      #li link_to "Add a New Tier", admin_click_table_ink_arrays_path(click_table.id)
+  end
 
 end
 
