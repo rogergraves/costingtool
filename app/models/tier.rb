4c467dcce49000017768f9aaf7feb04bffbec0b9
Roger Graves <rogergraves@gmail.com>
2013-05-21 16:34:36 -0700

    Added option :black_price to Tier model

--- app/models/tier.rb
+++ app/models/tier.rb
@@ -1,5 +1,5 @@
 class Tier < ActiveRecord::Base
-  attr_accessible :label, :price, :volume_range_end, :volume_range_start, :ink_array_id, :ink_array_name, :click_table_id, :click_table_name
+  attr_accessible :label, :price, :volume_range_end, :volume_range_start, :ink_array_id, :ink_array_name, :click_table_id, :click_table_name, :black_price
   belongs_to :ink_array
   delegate :click_table, :to => :ink_array, :allow_nil => true
 
@@ -9,5 +9,10 @@ class Tier < ActiveRecord::Base
   def tier_name
     self.label
   end
+
+  serialize :data, ActiveRecord::Coders::Hstore
+  hstore :data, :accessors => {
+      :black_price => :float,
+  }
 
 end
