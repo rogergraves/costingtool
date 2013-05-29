7da79244a7278e3158ee4d3ea6130976e599f0c5
Ben Johnston <benjohnstonsf@gmail.com>
2013-05-28 23:32:09 -0700

    overwrote mime types for icon deployment

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,8 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+
+Mime::Type.register "image/svg+xml", :svg
+Mime::Type.register "application/octet-stream", :ttf
+Mime::Type.register "application/octet-stream", :eot
