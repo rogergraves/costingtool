e24145d096512ff9ea168a2d6d83dd8c083c6f82
ea0723 <elenanewton@icloud.com>
2013-05-28 22:30:26 -0700

    updated media so name is a dropdown with media sizes instead of a text field

--- app/admin/media.rb
+++ app/admin/media.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Media do
 
   index :title => :name do
-    column :name
+      column :name
     column :cost_per_sheet
     default_actions
   end
@@ -15,8 +15,8 @@ ActiveAdmin.register Media do
 
   form do |f|
     f.inputs "Media" do
-      f.input :name
-      f.input :cost_per_sheet, :hint => "(cost per ream / sheets per ream)"
+        f.input :name, :as => :select, :collection => Job.available_sizes
+      f.input :cost_per_sheet, :hint => "cost per sheet = cost per ream / sheets per ream"
     end
     f.buttons
   end
