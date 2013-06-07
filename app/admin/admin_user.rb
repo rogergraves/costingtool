da0b2904459d63beab7eae1565f05da53ee4df4d
ea0723 <elenanewton@icloud.com>
2013-06-07 15:23:05 -0700

    cleaned up admin_users section in admin [finishes #51206301]

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -3,18 +3,29 @@ ActiveAdmin.register AdminUser do
     config.clear_sidebar_sections!
 
   index do                            
-    column :email                     
+    selectable_column
+    column("email"){ |email| link_to email.email, edit_admin_admin_user_path(email.id)}
     column :current_sign_in_at        
     column :last_sign_in_at           
-    column :sign_in_count             
-    default_actions                   
+    column :sign_in_count
+    #default_actions
   end                                 
+
+    show do
+      attributes_table do
+        row :email
+        row :current_sign_in_at
+        row :last_sign_in_at
+        row :sign_in_count
+        end
+        strong em { link_to "Update Administrator", edit_admin_admin_user_path(admin_user.id)}
+      end
 
   filter :email                       
 
   form do |f|                         
-    f.inputs "Admin Details" do       
-      f.input :email                  
+    f.inputs "Admin Details" do
+      f.input :email
       f.input :password               
       f.input :password_confirmation  
     end                               
